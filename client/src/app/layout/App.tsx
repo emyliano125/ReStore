65e87640d93aef157ef3dec23f67a0803f81cc9b
Dumitru Emilian <d_emilian@yahoo.com>
2023-04-04 14:48:28 +0300

    End of section 5

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,6 +3,8 @@ import { Container } from "@mui/system";
 import Header from "./Header";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
@@ -26,6 +28,7 @@ function handleThemeChange(){
 
   return (
     <ThemeProvider theme={theme}>
+<ToastContainer  position = "bottom-right" hideProgressBar theme="colored"/>
     <CssBaseline/>
      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
         <Container>
